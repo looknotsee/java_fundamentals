63fd69a1c7df2b2bc87f3008850c7c8687d18b21
looknotsee <lukenatsee@gmail.com>
2025-07-10 18:39:59 +0800

    update

--- src/syn2.java
+++ src/syn2.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class syn2 {
-    int Yo;
+    public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    //scanner object
+
+    System.out.print("Enter your name: ");
+    String name = sc.nextLine(); //.next won't read anything after a space
+    
+    System.out.println("Welcome, " + name);
+
+    System.out.print("Enter your age: ");
+    int age = sc.nextInt();
+
+    System.out.println("Your age of " + age + " has been taken note of.");
+
+    sc.close();
+    }
 }
---
+++ src/vars.java
@@ -0,0 +1,22 @@
+public class vars {
+    public static void main (String[] args) {
+
+    //reference type
+    String item = "Milk";
+
+    //primitive types
+    double price = 3.99;
+    char dollar = '$';
+    boolean forSale = true;
+
+    if (forSale) {
+        System.out.println(item + " costing " + dollar + price + " can be bought!");
+    } else {
+        System.out.println("No item available.");
+    }
+   
+       /*
+        use sout to insta print systemprint
+        */
+    }
+}
