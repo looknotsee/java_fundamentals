8067a026a3be0c19a0549b8c32e5145914365722
looknotsee <lukenatsee@gmail.com>
2025-07-10 20:59:38 +0800

    update

--- src/syn2.java
+++ src/syn2.java
@@ -7,13 +7,29 @@ public class syn2 {
 
     System.out.print("Enter your name: ");
     String name = sc.nextLine(); //.next won't read anything after a space
-    
-    System.out.println("Welcome, " + name);
 
     System.out.print("Enter your age: ");
     int age = sc.nextInt();
 
-    System.out.println("Your age of " + age + " has been taken note of.");
+    System.out.println("Your name is: " + name);
+    System.out.println("Your age is: " + age);
+
+    System.out.println("Are you a student? (True/False)");
+    boolean isStudent = sc.nextBoolean();
+
+    if (isStudent) {
+        System.out.println("Enter your GPA: ");
+        double GPA = sc.nextDouble();
+
+        if (GPA < 50 || GPA > 100) {
+            System.out.println("This GPA is invalid.");
+        } else {
+            System.out.println("Your grade of " + GPA + " has been taken note of. You may proceed to entrance A.");
+        }
+
+    } else {
+        System.out.println("Hi " + name + "! You may proceed to Entrance B.");
+    }
 
     sc.close();
     }
